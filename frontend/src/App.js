36358fdae7562f594e6e3c553c546e140cb35712
Amrut Sagar <amrutsagar06@gmail.com>
2022-09-08 00:24:14 +0530

    services

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>hi</BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
